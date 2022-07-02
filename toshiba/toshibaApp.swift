5470eff324c86b44c05655c24e1fcdcb18d77ec4
thefccompany <thefccompany@gmail.com>
2022-07-02 20:56:23 +0200

    Initial Commit

---
+++ toshiba/toshibaApp.swift
@@ -0,0 +1,17 @@
+//
+//  toshibaApp.swift
+//  toshiba
+//
+//  Created by Cyrille Fougerouse on 02/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct toshibaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
